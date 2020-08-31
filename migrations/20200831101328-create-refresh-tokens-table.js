8ede8a78bc4e083d99ccbe8c6b03a2993cd0a93d
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-31 19:51:24 +0700

    migration:setup refresh tokens table

--- migrations/20200831101328-create-refresh-tokens-table.js
+++ migrations/20200831101328-create-refresh-tokens-table.js
@@ -8,6 +8,40 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable('refresh_tokens', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.addConstraint('refresh_tokens', {
+      type: 'foreign key',
+      name: 'REFRESH_TOKENS_USER_ID',
+      fields: ['user_id'],
+      references: {
+        table: 'users',
+        field: 'id',
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -17,5 +51,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+    await queryInterface.dropTable('refresh_tokens');
+  },
 };
