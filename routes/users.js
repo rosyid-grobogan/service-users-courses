88a9cc9ee4e33cc15d724d7f513f8d935d5fd7fc
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-01 09:00:36 +0700

    API:login

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersHandler = require('./handler/users');
 
 router.post('/register', usersHandler.register);
+router.post('/login', usersHandler.login);
 
 module.exports = router;
