dfdfe0d36a3517a4130c994e6eb40de9f7011be3
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-01 17:31:10 +0700

    API:POST | logout

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const usersHandler = require('./handler/users');
 
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.post('/logout', usersHandler.logout);
 router.put('/:id', usersHandler.update);
 router.get('/:id', usersHandler.getUser);
 router.get('/', usersHandler.getUsers);
